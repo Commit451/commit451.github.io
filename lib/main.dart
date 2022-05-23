d1da915edaa28b4cde41682ce6fa3d5d48d68ec5
John Carlson <jawnnypoo@gmail.com>
2022-05-23 02:34:16 -0500

    Revert "Get rid of the # in the URL"
    This reverts commit 2944f9d6c672acba990c7d01bb0cce40395a273c.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:commit451/app.dart';
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() {
-  setPathUrlStrategy();
   runApp(const App());
 }
