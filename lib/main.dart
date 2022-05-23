2944f9d6c672acba990c7d01bb0cce40395a273c
John Carlson <jawnnypoo@gmail.com>
2022-05-23 02:27:10 -0500

    Get rid of the # in the URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:commit451/app.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const App());
 }
